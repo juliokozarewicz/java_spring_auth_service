dfc5e8aeeb71ef62b4726cc1480476d6ab235e24
Ed <julioerk@outlook.com>
2025-03-18 17:47:18 -0300

    FEATURE: Create Account development.

---
+++ accounts/src/main/java/com/example/demo/interfaces/AccountsEnableEmailInterface.java
@@ -0,0 +1,5 @@
+package com.example.demo.interfaces;
+
+public interface AccountsEnableEmailInterface {
+    void enableEmail(String activeId);
+}
--- accounts/src/main/java/com/example/demo/validations/AccountsCreateValidation.java
+++ accounts/src/main/java/com/example/demo/validations/AccountsCreateValidation.java
@@ -2,7 +2,7 @@ package com.example.demo.validations;
 
 import jakarta.validation.constraints.Size;
 
-public record HelloWorldValidation(
+public record AccountsCreateValidation(
 
         @Size(min=1, message="{not_empty}")
         @Size(max=100, message="{many_characters}")
