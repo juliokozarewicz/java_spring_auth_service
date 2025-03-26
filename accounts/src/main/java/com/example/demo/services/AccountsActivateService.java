49b4f72f66f484ce34e3a18e0ff2cc835120e912
Julio Kozarewicz <juliokozarewicz@gmail.com>
2025-03-26 10:21:28 -0300

    FEATURE: Create Account service documentation finished.

---
+++ accounts/src/main/java/com/example/demo/services/AccountsActivateService.java
@@ -0,0 +1,17 @@
+package com.example.demo.services;
+
+public class AccountsActivateService {
+    /*
+    Search for email and token.
+
+    Check if an email & token exist in the database.
+
+    If they exist and the account is deactivated, activate the account.
+
+    If they exist and the account is already activated, notify the user and ask them to change their password.
+
+    Delete all tokens associated with the email.
+
+    If no email & token exist, return a 404.
+    */
+}
