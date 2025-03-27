aa0416bc017d3ec30c2aa330da8fdcb34500de64
Julio Kozarewicz <juliokozarewicz@gmail.com>
2025-03-27 11:24:02 -0300

    FEATURE: Activate Account development.

--- accounts/src/main/java/com/example/demo/services/AccountsActivateService.java
+++ accounts/src/main/java/com/example/demo/services/AccountsActivateService.java
@@ -1,18 +1,106 @@
 package com.example.demo.services;
 
+import com.example.demo.exceptions.ErrorHandler;
+import com.example.demo.persistence.entities.AccountsEntity;
+import com.example.demo.persistence.entities.VerificationTokenEntity;
+import com.example.demo.persistence.repositories.AccountsRepository;
+import com.example.demo.persistence.repositories.VerificationTokenRepository;
+import com.example.demo.utils.StandardResponse;
+import com.example.demo.validations.AccountsActivateValidation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.ResponseEntity;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+
 public class AccountsActivateService {
     /*
-    Search for email and token.
-
-    Check if an email & token exist in the database.
-
-    If they exist and the account is deactivated, activate the account.
-
-    If they exist and the account is already activated, notify the user and ask them to change their password.
+    If an email & token exist and the account is deactivated, activate the account.
 
     Delete all tokens associated with the email.
-
-    If no email & token exist, return a 404.
     */
+
+    // attributes
+    @Value("${APPLICATION_TITLE}")
+    private String applicatonTitle;
+
+    private final MessageSource messageSource;
+    private final AccountsManagementService accountsManagementService;
+    private VerificationTokenRepository verificationTokenRepository;
+    private ErrorHandler errorHandler;
+    private AccountsRepository accountsRepository;
+
+    // constructor
+    public AccountsActivateService (
+        MessageSource messageSource,
+        ErrorHandler errorHandler,
+        AccountsManagementService accountsManagementService,
+        VerificationTokenRepository verificationTokenRepository,
+        AccountsRepository accountsRepository
+    ) {
+        this.messageSource = messageSource;
+        this.errorHandler = errorHandler;
+        this.accountsManagementService = accountsManagementService;
+        this.verificationTokenRepository = verificationTokenRepository;
+        this.accountsRepository = accountsRepository;
+    }
+
+    public ResponseEntity execute(
+        AccountsActivateValidation accountsActivateValidation
+    ) {
+
+        // language
+        Locale locale = LocaleContextHolder.getLocale();
+
+        // find email and token
+        Optional<VerificationTokenEntity> findEmailAndToken =
+            verificationTokenRepository.findByEmailAndToken(
+                accountsActivateValidation.email().toLowerCase(),
+                accountsActivateValidation.token()
+            );
+
+        // find user
+        Optional<AccountsEntity> findUser =  accountsRepository.findByEmail(
+            accountsActivateValidation.email().toLowerCase()
+        );
+
+        // email & token or account not exist
+        if ( findEmailAndToken.isEmpty() || findUser.isEmpty() ) {
+            // call custom error
+            errorHandler.customErrorThrow(
+                404,
+                messageSource.getMessage(
+                    "activate_email_error", null, locale
+                )
+            );
+        }
+
+        // response (links)
+        // ---------------------------------------------------------------------
+        Map<String, String> customLinks = new LinkedHashMap<>();
+        customLinks.put("self", "/accounts/activate-email");
+        customLinks.put("next", "/accounts/login");
+
+        StandardResponse response = new StandardResponse.Builder()
+            .statusCode(201)
+            .statusMessage("success")
+            .message(
+                messageSource.getMessage(
+                    "email_activate",
+                    null,
+                    locale
+                )
+            )
+            .links(customLinks)
+            .build();
+        return ResponseEntity
+            .status(response.getStatusCode())
+            .body(response);
+        // ---------------------------------------------------------------------
+
+    };
 
 }
---
+++ accounts/src/main/java/com/example/demo/validations/AccountsActivateValidation.java
@@ -0,0 +1,26 @@
+package com.example.demo.validations;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record AccountsActivateValidation(
+
+        @NotEmpty(message = "{is_required}")
+        @Size(max = 255, message = "{many_characters}")
+        @Email(message = "{must_be_a_valid_email}")
+        String email,
+
+        @NotEmpty(message = "{is_required}")
+        @Pattern(
+                regexp = "^[a-fA-F0-9]{64}$",
+                message = "{contains_disallowed_characters}"
+        )
+        @Pattern(
+            regexp = "^[^<>&'\"/]+$",
+            message = "{contains_disallowed_characters}"
+        )
+        String token
+
+) {}
