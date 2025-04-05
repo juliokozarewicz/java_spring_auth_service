73afaf371fbee46de560f28b4978fab468d0ae87
Ed <julioerk@outlook.com>
2025-04-05 12:45:31 -0300

    FEATURE: Login initialized.

---
+++ accounts/src/main/java/com/example/demo/validations/AccountsLoginValidation.java
@@ -0,0 +1,30 @@
+package com.example.demo.validations;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record AccountsLoginValidation(
+
+        @NotEmpty(message = "{validation_is_required}")
+        @Size(max = 255, message = "{validation_many_characters}")
+        @Email(message = "{validation_must_valid_email}")
+        String email,
+
+        @NotEmpty(message = "{validation_is_required}")
+        @Size(
+                min = 8, max = 255,
+                message = "{validation_must_eight_characters_long}"
+        )
+        @Pattern(
+                regexp = "^[^<>&'\"/]+$",
+                message = "{validation_disallowed_characters}"
+        )
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$",
+                message = "{validation_must_uppercase_letter}"
+        )
+        String password
+
+) {}
