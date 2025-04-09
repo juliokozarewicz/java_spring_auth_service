39afeb30aed616479fc37b16f88decbf95e59fd7
Ed <julioerk@outlook.com>
2025-04-09 18:23:30 -0300

    FEATURE: Login initialized.

---
+++ accounts/src/main/java/com/example/demo/controllers/AccountsLoginController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controllers;
+
+import com.example.demo.services.AccountsLoginService;
+import com.example.demo.validations.AccountsLoginValidation;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+@Validated
+class AccountsLoginController {
+
+    // Service
+    private final AccountsLoginService accountsLoginService;
+
+    // constructor
+    public AccountsLoginController(
+        AccountsLoginService accountsLoginService
+    ) {
+        this.accountsLoginService = accountsLoginService;
+    }
+
+    @PostMapping("${BASE_URL_ACCOUNTS}/login")
+    public ResponseEntity handle(
+
+        // validations errors
+        @Valid @RequestBody AccountsLoginValidation accountsLoginValidation,
+        HttpServletRequest request,
+        BindingResult bindingResult
+
+    ) {
+
+        String userIp = request.getRemoteAddr();
+        String userAgent = request.getHeader("User-Agent");
+
+        return accountsLoginService.execute(
+            userIp,
+            userAgent,
+            accountsLoginValidation
+        );
+
+    }
+
+}
