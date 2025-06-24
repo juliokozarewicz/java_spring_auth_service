6f50728fc57e8574da567ecc119b2efbd9905d7d
Julio Kozarewicz <juliokozarewicz@gmail.com>
2025-06-24 11:44:33 -0300

    FEATURE: Endpoint auth finished.

--- helloworld/src/main/java/com/example/demo/configurations/CorsConfig.java
+++ helloworld/src/main/java/com/example/demo/configurations/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${PUBLIC_DOMAIN}")
+    @Value("http://${PUBLIC_DOMAIN}")
     private String publicDomain;
 
     @Override
