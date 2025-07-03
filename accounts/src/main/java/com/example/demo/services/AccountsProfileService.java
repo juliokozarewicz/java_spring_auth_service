a4f562dfcdeef1d1a93a75a082706cdcbdb62185
Julio Kozarewicz <julioerk@outlook.com>
2025-07-03 12:32:37 -0300

    FEATURE: Profile endpoint development.

--- accounts/src/main/java/com/example/demo/services/AccountsProfileService.java
+++ accounts/src/main/java/com/example/demo/services/AccountsProfileService.java
@@ -1,12 +1,19 @@
 package com.example.demo.services;
 
 import com.example.demo.exceptions.ErrorHandler;
+import com.example.demo.persistence.entities.AccountsEntity;
+import com.example.demo.persistence.entities.AccountsProfileEntity;
+import com.example.demo.persistence.repositories.ProfileRepository;
 import com.example.demo.utils.StandardResponse;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedHashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class AccountsProfileService {
@@ -14,29 +21,53 @@ public class AccountsProfileService {
     // attributes
     private final MessageSource messageSource;
     private final ErrorHandler errorHandler;
+    private final ProfileRepository profileRepository;
 
     // constructor
     public AccountsProfileService(
 
         MessageSource messageSource,
-        ErrorHandler errorHandler
+        ErrorHandler errorHandler,
+        ProfileRepository profileRepository
 
     ) {
 
         this.messageSource = messageSource;
         this.errorHandler = errorHandler;
+        this.profileRepository = profileRepository;
 
     }
 
-    public ResponseEntity execute() {
+    public ResponseEntity execute(
+
+        Map<String, String> credentialsData
+
+    ) {
 
         // language
         Locale locale = LocaleContextHolder.getLocale();
 
+        // Credentials
+        String idUser = credentialsData.get("id").toString();
+        String emailUser = credentialsData.get("email").toString();
+        String levelUser = credentialsData.get("level").toString();
+
+        // find user
+        Optional<AccountsProfileEntity> findProfileUser =  profileRepository.findById(
+            idUser
+        );
+
+        // Links
+        Map<String, String> customLinks = new LinkedHashMap<>();
+        customLinks.put("self", "/accounts/profile");
+        customLinks.put("next", "/accounts/profile-update");
+
         // Response
         StandardResponse response = new StandardResponse.Builder()
             .statusCode(200)
-            .statusMessage("success_xxx")
+            .statusMessage("success")
+            .data(findProfileUser)
+            .links(customLinks)
             .build();
 
         return ResponseEntity
