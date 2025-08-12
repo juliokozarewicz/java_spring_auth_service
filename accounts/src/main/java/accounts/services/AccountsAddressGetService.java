930d36f456661327ba1fab726122ca974fbf7b5b
Julio Kozarewicz <juliokozarewicz@gmail.com>
2025-08-12 12:00:02 -0300

    FEATURE(get address): Service finished.

--- accounts/src/main/java/accounts/services/AccountsAddressGetService.java
+++ accounts/src/main/java/accounts/services/AccountsAddressGetService.java
@@ -5,6 +5,9 @@ import accounts.dtos.AccountsProfileDTO;
 import accounts.exceptions.ErrorHandler;
 import accounts.persistence.entities.AccountsAddressEntity;
 import accounts.persistence.repositories.AccountsAddressRepository;
+import accounts.persistence.repositories.AccountsProfileRepository;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
@@ -19,19 +22,25 @@ public class AccountsAddressGetService {
     private final MessageSource messageSource;
     private final ErrorHandler errorHandler;
     private final AccountsAddressRepository accountsAddressRepository;
+    private final CacheManager cacheManager;
+    private final Cache jwtCache;
 
     // constructor
     public AccountsAddressGetService(
 
         MessageSource messageSource,
         ErrorHandler errorHandler,
-        AccountsAddressRepository accountsAddressRepository
+        AccountsAddressRepository accountsAddressRepository,
+        AccountsProfileRepository accountsProfileRepository,
+        CacheManager cacheManager
 
     ) {
 
         this.messageSource = messageSource;
         this.errorHandler = errorHandler;
         this.accountsAddressRepository = accountsAddressRepository;
+        this.cacheManager = cacheManager;
+        this.jwtCache = cacheManager.getCache("addressCache");
 
     }
 
@@ -53,27 +62,40 @@ public class AccountsAddressGetService {
 
         // Redis cache ( get or set )
         // =================================================================
-        List<AccountsAddressEntity> findAddress = accountsAddressRepository
-            .findByUserId(idUser);
+        Cache.ValueWrapper cached = jwtCache.get(idUser);
 
-        for (AccountsAddressEntity entity : findAddress) {
+        if ( cached != null ) {
 
-            AccountsAddressGetDTO dto = new AccountsAddressGetDTO();
+            dtoAddressList = (List<AccountsAddressGetDTO>) cached.get();
 
-            dto.setAddressId(entity.getId());
-            dto.setZipCode(entity.getZipCode());
-            dto.setStreet(entity.getStreet());
-            dto.setNumber(entity.getNumber());
-            dto.setAddressLineTwo(entity.getAddressLineTwo());
-            dto.setNeighborhood(entity.getNeighborhood());
-            dto.setCity(entity.getCity());
-            dto.setState(entity.getState());
-            dto.setCountry(entity.getCountry());
-            dto.setAddressType(entity.getAddressType());
-            dto.setIsPrimary(entity.getIsPrimary());
-            dto.setLandmark(entity.getLandmark());
+        } else {
 
-            dtoAddressList.add(dto);
+            List<AccountsAddressEntity> findAddress = accountsAddressRepository
+                .findByUserId(idUser);
+
+            for (AccountsAddressEntity entity : findAddress) {
+
+                AccountsAddressGetDTO dto = new AccountsAddressGetDTO();
+
+                dto.setAddressId(entity.getId());
+                dto.setZipCode(entity.getZipCode());
+                dto.setStreet(entity.getStreet());
+                dto.setNumber(entity.getNumber());
+                dto.setAddressLineTwo(entity.getAddressLineTwo());
+                dto.setNeighborhood(entity.getNeighborhood());
+                dto.setCity(entity.getCity());
+                dto.setState(entity.getState());
+                dto.setCountry(entity.getCountry());
+                dto.setAddressType(entity.getAddressType());
+                dto.setIsPrimary(entity.getIsPrimary());
+                dto.setLandmark(entity.getLandmark());
+
+                dtoAddressList.add(dto);
+
+            }
+
+            jwtCache.put(idUser, dtoAddressList);
+
         }
         // =================================================================
 
